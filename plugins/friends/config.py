7f46444982258bb2f24dee579e33dc4e59791fde
FrostN0v0 <1614591760@qq.com>
2024-11-07 18:04:18 +0800

    💥 Boom!

--- plugins/friends/config.py
+++
@@ -1,8 +0,0 @@
-from kirami.config import BaseConfig
-from kirami.config import bot_config
-
-
-class Config(BaseConfig):
-    auto_add_friends: bool = True
-    """自动同意添加好友"""
-    master_id: list[str] = list(bot_config.superusers)
--- plugins/image/__init__.py
+++
@@ -1,8 +0,0 @@
-from .new_Catalog import *
-from .upload_img import *
-from .delete_img import *
-from .send_img import *
-from kirami.server import Server
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
